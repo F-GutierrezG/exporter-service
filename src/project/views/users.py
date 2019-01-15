05ae2be31f42f4a992b6c976fd42209ed4860c12
Francisco Gutierrez <fgutierrez@grouponlatam.com>
2019-01-15 16:40:05 -0300

    Added auth lib dependency

--- src/project/views/users.py
+++ src/project/views/users.py
@@ -2,11 +2,14 @@ from flask import Blueprint, Response
 
 from project.logics import UserLogics
 
+from auth.decorators import authenticate
+
 
 users_blueprint = Blueprint('users', __name__)
 
 
 @users_blueprint.route('/exporter/users', methods=['GET'])
+@authenticate
 def health():
     csv = UserLogics().admins_csv()
     return Response(csv, mimetype="text/csv")

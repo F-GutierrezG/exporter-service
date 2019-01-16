2df96c0bdad06a4caa501e889fcea5b812c65954
Carlos Silva <anklosr@gmail.com>
2019-01-15 21:15:03 -0300

    Se agregan campos en la exportacion de campos de publicacion

--- src/project/logics.py
+++ src/project/logics.py
@@ -148,7 +148,7 @@ class PublicationsLogics:
         return file.getvalue()
 
     def __get_publication_header(self):
-        return ['ID', 'TITLE', 'MESSAGE', 'RED SOCIAL', 'TAG']
+        return ['ID', 'TITLE','ID COMPANIA','TEXTO ADICIONAL','CONTENIDO','REFERENCIA RED SOCIAL','MESSAGE','FECHA DE PUBLICACION','FECHA CREACION','CREADO POR', 'RED SOCIAL', 'TAG']
 
     def __get_row(self, publication, network):
         row = []
@@ -166,8 +166,16 @@ class PublicationsLogics:
 
         row.append(publication['id'])
         row.append(publication['title'])
+        row.append(publication['company_id'])
+        row.append(publication['additional'])
+        row.append(publication['image_url'])
+        row.append(publication['link'])      
         row.append(publication['message'])
+        row.append(publication['date'])
+        row.append(publication['created'])
+        row.append(publication['created_by'])
         row.append(network)
         row.append(tag)
+
 
         return row

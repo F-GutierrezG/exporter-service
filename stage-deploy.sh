89174e5772bedb235eb823b67485b84448c1f992
Francisco Gutierrez <fgutierrez@grouponlatam.com>
2019-01-18 14:06:46 -0300

    Deploy environment variables

--- stage-deploy.sh
+++ stage-deploy.sh
@@ -13,7 +13,7 @@ ssh -o StrictHostKeyChecking=no ubuntu@${STAGE_SERVER} 'docker image rm $(docker
 ssh -o StrictHostKeyChecking=no ubuntu@${STAGE_SERVER} 'docker image rm $(docker images registry.gitlab.com/gusisoft/onelike/exporter-service/exporter -q)'
 
 ssh -o StrictHostKeyChecking=no ubuntu@${STAGE_SERVER} "docker run -d --restart always -e 'API_URL=definitions/swagger.yml' --name exporter-swagger --network exporter-service-network --ip 172.24.0.3 $REGISTRY_REPO/$SWAGGER:$TAG"
-ssh -o StrictHostKeyChecking=no ubuntu@${STAGE_SERVER} "docker run -d --restart always -e 'FLASK_ENV=development' -e 'FLASK_APP=manage.py' -e 'APP_SETTINGS=project.config.DevelopmentConfig' -e 'SECRET_KEY=secret_key' -e USERS_SERVICE_URL=$USERS_SERVICE_URL -e AUTH_SERVICE_URL=$AUTH_SERVICE_URL --name exporter --network exporter-service-network --ip 172.24.0.2 $REGISTRY_REPO/$EXPORTER:$TAG"
+ssh -o StrictHostKeyChecking=no ubuntu@${STAGE_SERVER} "docker run -d --restart always -e 'FLASK_ENV=development' -e 'FLASK_APP=manage.py' -e 'APP_SETTINGS=project.config.DevelopmentConfig' -e 'SECRET_KEY=secret_key' -e USERS_SERVICE_URL=$USERS_SERVICE_URL -e AUTH_SERVICE_URL=$AUTH_SERVICE_URL -e COMPANIES_SERVICE_URL=$COMPANIES_SERVICE_URL -e SOCIAL_SERVICE_URL=$SOCIAL_SERVICE_URL --name exporter --network exporter-service-network --ip 172.24.0.2 $REGISTRY_REPO/$EXPORTER:$TAG"
 
 ssh -o StrictHostKeyChecking=no ubuntu@${STAGE_SERVER} 'docker network connect onelike-network --ip 172.18.0.14 exporter'
 ssh -o StrictHostKeyChecking=no ubuntu@${STAGE_SERVER} 'docker network connect onelike-network --ip 172.18.0.15 exporter-swagger'
